e3957b8592682a9cb7be43c19bc90081b5ae5a3f
KephaModicz <125441027+KephaModicz@users.noreply.github.com>
2025-12-07 20:23:45 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "eulo_kims";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("DB Connection failed: " . $conn->connect_error);
+}
+?>
 
